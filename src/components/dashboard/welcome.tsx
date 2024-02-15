20e73af3682ecc74951887940fa0e1e5c8eac5c6
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-02-15 11:19:54 +0530

    fixed more issues

--- src/components/dashboard/welcome.tsx
+++ src/components/dashboard/welcome.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { Box, Button, Divider, Grid, Stack, Typography } from "@mui/material";
 import FormDialog from "./search";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 type Props = {
   name: string;
@@ -8,37 +9,47 @@ type Props = {
 };
 
 export const Welcome = ({ name, data }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
   return (
-    <main>
-      <Grid container spacing={2} sx={{ width: "100%", mx: 0 }}>
-        <Grid xs={12} sm={12}>
-          <Typography variant="h3" align="center">
-            Welcome
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          <Typography variant="h5" align="center" sx={{ mt: 1 }}>
-            Dr .xxxxxx
-          </Typography>
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={12} md={12}>
+        <Typography variant="h3" align="center">
+          Welcome
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12}>
+        <Typography variant="h5" align="center" sx={{ mt: 1 }}>
+          {name}
+        </Typography>
       </Grid>
 
-      <Divider sx={{ my: 2 }} />
-      <Box bgcolor={"secondary.main"} sx={{ p: 4 }}>
-        <Grid container spacing={2}>
-          {data.map((item, index) => (
-            <Fragment key={index}>
-              <Grid item xs={6}>
-                <Typography>{item.left}</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography>: {item.right}</Typography>
-              </Grid>
-            </Fragment>
-          ))}
+      <Grid container item xs={12} sm={12} md={12}>
+        <Grid item xs={12} sm={12} md={12}>
+          <Divider />
         </Grid>
-      </Box>
-      <FormDialog />
-    </main>
+        {data.map((item, index) => (
+          <Fragment key={index}>
+            <Grid item xs={6} sm={6} md={6} textAlign={"center"}>
+              <Typography>{item.left}</Typography>
+            </Grid>
+            <Grid item xs={6} sm={6} md={6}>
+              <Typography>: {item.right}</Typography>
+            </Grid>
+          </Fragment>
+        ))}
+      </Grid>
+
+      <Grid item xs={12} sm={12} md={12} textAlign={"center"}>
+        <Button variant="contained" onClick={handleClick}>
+          Search Patient
+        </Button>
+      </Grid>
+      <FormDialog open={open} setOpen={setOpen} />
+    </Grid>
   );
 };
